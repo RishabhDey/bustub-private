10da9252bc1c73f25b2ac9deeb15f5af29bc5e5d
Benjamin Berg <ben.berg781@gmail.com>
2025-08-08 15:59:10 -0400

    docker config updates

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
+if [[ -z $(docker images --format json bustub:unc) ]] 
+then
+    ./build_support/docker.sh
+fi
+
 if [[ -z $(docker ps | grep bustub:unc) ]]
 then
     CONTAINER_ID=$(docker run -d -v bustub-volume:/workspace/bustub-unc bustub:unc)
+else
+    CONTAINER_ID=$(docker ps -f ancestor=bustub:unc --format="{{.ID}}")
 fi
 docker exec -it -w /workspace/bustub-unc "${CONTAINER_ID}" /bin/bash
